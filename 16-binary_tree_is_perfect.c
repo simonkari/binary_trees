7ec2e1ac3e2d7f5f71d6f8364c3842fbcd019b0e
simonkari <kariithisimon1715@gmail.com>
2023-08-03 20:44:32 +0300

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,10 +1,13 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_perfect- is tree perfect?  all interior nodes have two
+ * binary_tree_is_perfect- This function checks if a tree is perfect,
+ * meaning all interior nodes have two
  * children and all leaves have the same depth or same level
- * @tree: Pointer to the root node of the tree to check.
- * Return: 1 if the tree is perfect, 0 otherwise.
+ * @tree:  pointer to the root of the tree.
+ * Return: 1 if the tree is perfect, 0 if it is not.
  */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 /* if tree is null return 0 */
@@ -23,10 +26,9 @@ if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
 return (0);
 }
 /**
- * binary_tree_height - get the height of tree
- * @tree: the tree to measure
+ * binary_tree_height - Get the height of tree
+ * @tree: The tree to measure
  *Return: size_t height or 0
- *
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
