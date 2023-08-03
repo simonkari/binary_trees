17fa3aca9160fcd860b39cd6f7e492c4058735af
simonkari <kariithisimon1715@gmail.com>
2023-08-03 18:25:48 +0300

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,15 +1,17 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_leaf- check if node is end point
- * @node: the node to check
+ * binary_tree_is_leaf - Check if a node is a leaf node.
+ * @node: The node to check.
  *
- * Return: 1 if leaf, otherwise 0
+ * Return: 1 if the node is a leaf, otherwise 0.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	/* if node is null return 0 */
-	if (!node)
-		return (0);
-	/*otherwise return true if neither left or right leaves exist */
-	return ((!node->left) && (!node->right));
+    /* If the node is null, it cannot be a leaf. */
+    if (!node)
+        return (0);
+
+    /* Return true (1) if neither left nor right children exist. */
+    return (node->left == NULL && node->right == NULL);
 }
