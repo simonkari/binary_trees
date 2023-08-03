39dd6a66195009ad6dc279bafd0d6b33004b8c4e
simonkari <kariithisimon1715@gmail.com>
2023-08-03 18:16:54 +0300

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,33 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - insert a node as the left-child of another node
- * @parent: ptr to parent of the node to create
- * @value: value to insert in the new node
- *
- * Return: ptr to the new node or NULL if memory allocation fails
- */
+ * binary_tree_insert_left - Creates a new node and inserts
+ * it as the left-child of a given parent node.
+ * @parent: A pointer to the parent node.
+ * @value: The value to be inserted into the new node.
+ * Return: A pointer to the new node if successful, or NULL if the insertion fails.
+*/
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (!parent)
-        return (NULL);
+	/* declarations */
+	binary_tree_t *node = NULL;
 
-    /* Allocate memory for the new node */
-    binary_tree_t *node = calloc(1, sizeof(binary_tree_t));
-    if (!node)
-        return (NULL);
-
-    /* Initialize the new node */
-    node->n = value;
-    node->parent = parent;
-
-    /* Check if parent already has a left child */
-    if (parent->left)
-    {
-        node->left = parent->left;
-        node->left->parent = node;
-    }
-
-    parent->left = node;
-    return (node);
+	/* check that parent is not NULL */
+	if (!parent)
+		return (NULL);
+	/* calloc for node */
+	node = calloc(1, sizeof(binary_tree_t));
+	/* check for calloc fail */
+	if (!node)
+		return (NULL);
+	/* assign things */
+	node->n = value;
+	node->parent = parent;
+	/* check for existing left child */
+	if (parent->left)
+	{
+		node->left = parent->left;
+		node->left->parent = node;
+	}
+	parent->left = node;
+	return (node);
 }
